31cb5ad0faacf9dc5092b5532f5df892a491a3cf
Matt <sphitzer@gmail.com>
2023-03-30 15:29:12 -0400

    Lots o updates

--- img-processor/src/main.py
+++ img-processor/src/main.py
@@ -1,26 +1,26 @@
-import sys, os, time 
+import sys, os, time
 import argparse
-import json
-
-#replace w/ preferred logging tool loguru
 import logging
-import logger
-
+from loguru import logger
 import ffmpeg
-import openai
 import boto3
 from botocore.exceptions import ClientError
 from dotenv import load_dotenv
-import requests
 import mux_python
+from confluent_kafka import Consumer, KafkaError
+from io import BytesIO
+from PIL import Image
+import tempfile
+import shutil
+import logging
+#import kafka
+import base64
 
-# load environment variables
+
+# Load environment variables
 load_dotenv()
 
-#open ai auth/initialization setup
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-#mux auth setup
+# Mux auth setup
 configuration = mux_python.Configuration()
 configuration.username = os.environ['MUX_ACCESSS_TOKEN_ID']
 configuration.password = os.environ['MUX_SECRET_KEY']
@@ -29,89 +29,175 @@ configuration.password = os.environ['MUX_SECRET_KEY']
 assets_api = mux_python.AssetsApi(mux_python.ApiClient(configuration))
 playback_ids_api = mux_python.PlaybackIDApi(mux_python.ApiClient(configuration))
 
-def openai_test(image_path):
-    response = openai.Image.create_edit(
-        image=open()
-    )
+#
+THRESHOLD = 100
 
-    print(str(openai.Model.list()))
-    return None
+#Kafka log listener
+# kafka_logger = logging.getLogger("kafka")
+# kafka_logger.setLevel(logging.DEBUG)
+
+# handler = logging.StreamHandler()
+# handler.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+# kafka_logger.addHandler(handler)
+
+
+class TimelapseGenerator:
+    def __init__(self, name, timelapse_dir, s3bucket, kafka_bootstrap_servers):
+        self.name = name
+        self.timelapse_dir = timelapse_dir
+        self.s3bucket = s3bucket
+        self.kafka_bootstrap_servers = kafka_bootstrap_servers
+        self.consumer = self.create_consumer("timelapse_consumer_group1", "timelapse_images2" )
+
+    def generate_video_from_photos(self, image_file_paths, video_name_counter):
+        capture_parent_dir = os.path.join(self.timelapse_dir, self.name)
+        file_output_name = self.name
+        output_path = os.path.join(capture_parent_dir, 'output', f"{file_output_name}{video_name_counter}.mp4")
+
+        # Create a temporary directory to store the images
+        with tempfile.TemporaryDirectory() as temp_dir:
+            # Copy the images to the temporary directory
+            for idx, img_path in enumerate(image_file_paths):
+                shutil.copy(img_path, os.path.join(temp_dir, f'image_{idx:04d}.jpg'))
+
+            # Use the temporary directory as input for ffmpeg
+            (
+                ffmpeg
+                .input(os.path.join(temp_dir, '*.jpg'), pattern_type='glob', framerate=30)
+                .filter('deflicker', mode='pm', size=10)
+                .filter('scale', size='3840x2160', force_original_aspect_ratio='increase')
+                .output(output_path, crf=20, preset='slow', movflags='faststart', pix_fmt='yuv420p')
+                .run(overwrite_output=True)
+            )
+
+        return output_path
+
+    def upload_to_s3(self, file_path, object_name=None):
+        logger.info("Starting s3 upload")
+        if object_name is None:
+            object_name = os.path.basename(file_path)
+
+        s3_client = boto3.client('s3')
+        try:
+            response = s3_client.upload_file(file_path, self.s3bucket, object_name)
+        except ClientError as e:
+            logger.error(e)
+            logger.info("Failed s3 upload")
+            return False
+        
+        logger.info("Completed s3 upload")
+        return True
     
 
-def generate_video_from_photos(capture_parent_dir, file_output_name):
 
-    (
-        ffmpeg
-        .input(capture_parent_dir + '/capture/*.jpg', pattern_type='glob', framerate=30)
-        .filter('deflicker', mode='pm', size=10)
-        .filter('scale', size='3840x2160', force_original_aspect_ratio='increase')
-        .output(capture_parent_dir + '/output/' + file_output_name + '.mp4', crf=20, preset='slow', movflags='faststart', pix_fmt='yuv420p')
-        #.view(filename='filter_graph')
-        .run()
-    )
+    def mux_download_from_s3(self, file_name, bucket_name):
+        # https://github.com/muxinc/mux-python/blob/master/examples/video/exercise-assets.py
+        # see this for reference 
 
-    return capture_parent_dir + '/output/' + file_output_name + '.mp4'
+        input_settings = [mux_python.InputSettings(url=f'https://{bucket_name}.s3.us-east-2.amazonaws.com/{file_name}.mp4')]
+        create_asset_request = mux_python.CreateAssetRequest(input=input_settings)
+        create_asset_response = assets_api.create_asset(create_asset_request)
+        assert create_asset_response != None
+        assert create_asset_response.data.id != None
+        logger.info("create-asset OK ✅")
+
+        # Wait for the asset to become ready...
+        if create_asset_response.data.status != 'ready':
+            logger.info("    waiting for asset to become ready...")
+            while True:
+                # ========== get-asset ==========
+                asset_response = assets_api.get_asset(create_asset_response.data.id)
+                assert asset_response != None
+                assert asset_response.data.id == create_asset_response.data.id
+                if asset_response.data.status != 'ready':
+                    #logger.print_debug("Asset still not ready. Status was: " + asset_response.data.status)
+                    time.sleep(1)
+                else:
+                    # ========== get-asset-input-info ==========
+                    #logger.print_debug("Asset Ready. Checking input info.")
+                    get_asset_input_info_response = assets_api.get_asset_input_info(create_asset_response.data.id)
+                    #logger.print_debug("Got Asset Input Info: " + str(get_asset_input_info_response))
+                    assert get_asset_input_info_response != None
+                    assert get_asset_input_info_response.data != None
+                    break
+        logger.info("get-asset OK ✅")
+        logger.info("get-asset-input-info OK ✅")
+
+        # ========== create-asset-playback-id ==========
+        create_playback_id_request = mux_python.CreatePlaybackIDRequest(policy=mux_python.PlaybackPolicy.PUBLIC)
+        create_asset_playback_id_response = assets_api.create_asset_playback_id(create_asset_response.data.id, create_playback_id_request)
+        #logger.print_debug("Added Playback ID: " + str(create_asset_playback_id_response))
+        assert create_asset_playback_id_response != None
+        assert create_asset_playback_id_response.data != None
+        logger.info(str(create_asset_playback_id_response.data.id))
+        logger.info("create-asset-playback-id OK ✅")
+        return create_asset_playback_id_response.data.id
+
+    def create_consumer(self, group_id, topic):
+        conf = {
+            'bootstrap.servers': self.kafka_bootstrap_servers,
+            'group.id': group_id,
+            'auto.offset.reset': 'earliest',
+        }
+
+        consumer = Consumer(conf)
+        consumer.subscribe([topic])
+
+        return consumer
 
 
-def upload_to_s3(file_path, bucket, object_name=None):
+    def save_decoded_image_to_disk(self, img_data, image_number):
+        decoded_data = base64.b64decode(img_data)
+        img = Image.open(BytesIO(decoded_data))
+        
+        temp_dir = tempfile.gettempdir()
+        img_file_path = os.path.join(temp_dir, f'image_{image_number}.jpg')
+        
+        img.save(img_file_path, "JPEG")
+        
+        return img_file_path
 
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = os.path.basename(file_path)
+    def process_messages(self, threshold):
+        video_name_counter = 0
+        counter = 0
+        images = []
 
-    # Upload the file
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file(file_path, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
-
-#mux takes the file uploaded to s3 and processes it for streaming
-#function returns 
-def mux_download_from_s3(bucket_name, file_name):
-    # https://github.com/muxinc/mux-python/blob/master/examples/video/exercise-assets.py
-    # see this for reference 
-
-    input_settings = [mux_python.InputSettings(url=f'https://{bucket_name}.s3.us-east-2.amazonaws.com/{file_name}.mp4')]
-    create_asset_request = mux_python.CreateAssetRequest(input=input_settings)
-    create_asset_response = assets_api.create_asset(create_asset_request)
-    assert create_asset_response != None
-    assert create_asset_response.data.id != None
-    print("create-asset OK ✅")
-
-    # Wait for the asset to become ready...
-    if create_asset_response.data.status != 'ready':
-        print("    waiting for asset to become ready...")
         while True:
-            # ========== get-asset ==========
-            asset_response = assets_api.get_asset(create_asset_response.data.id)
-            assert asset_response != None
-            assert asset_response.data.id == create_asset_response.data.id
-            if asset_response.data.status != 'ready':
-                #logger.print_debug("Asset still not ready. Status was: " + asset_response.data.status)
-                time.sleep(1)
-            else:
-                # ========== get-asset-input-info ==========
-                #logger.print_debug("Asset Ready. Checking input info.")
-                get_asset_input_info_response = assets_api.get_asset_input_info(create_asset_response.data.id)
-                #logger.print_debug("Got Asset Input Info: " + str(get_asset_input_info_response))
-                assert get_asset_input_info_response != None
-                assert get_asset_input_info_response.data != None
-                break
-    print("get-asset OK ✅")
-    print("get-asset-input-info OK ✅")
+            logger.info(f"Polling messages (iteration {counter})...") 
+            msg = self.consumer.poll(4.0)
 
-    # ========== create-asset-playback-id ==========
-    create_playback_id_request = mux_python.CreatePlaybackIDRequest(policy=mux_python.PlaybackPolicy.PUBLIC)
-    create_asset_playback_id_response = assets_api.create_asset_playback_id(create_asset_response.data.id, create_playback_id_request)
-    #logger.print_debug("Added Playback ID: " + str(create_asset_playback_id_response))
-    assert create_asset_playback_id_response != None
-    assert create_asset_playback_id_response.data != None
-    print(str(create_asset_playback_id_response.data.id))
-    print("create-asset-playback-id OK ✅")
-    return create_asset_playback_id_response.data.id
+            if msg is None:
+                continue
+
+            
+            if msg.error():
+                logger.error (f"Msg: {counter} error while consuming message: {msg.error()} ")
+                continue
+
+            logger.info(f"Consumed message {counter}")
+            img_file_path = self.save_decoded_image_to_disk(msg.value(), len(images))
+            images.append(img_file_path)
+            logger.info(f"appended image {counter}")
+
+            counter += 1
+
+            if counter >= threshold:
+                video_name_counter += 1
+                logger.info(str(video_name_counter))
+                video_path = self.generate_video_from_photos(images, video_name_counter)
+                self.upload_to_s3(video_path)
+                asset_playback_id = self.mux_download_from_s3(f"{self.name}{video_name_counter}", self.s3bucket)
+
+                #reset for next timelapse
+                images = []
+                counter = 0
+                    
+    def run(self):
+        self.process_messages(THRESHOLD)
+
+        # video_path = self.generate_video_from_photos()
+        # self.upload_to_s3(video_path)
+        # asset_playback_id = self.mux_download_from_s3(self.name, self.s3bucket)
 
 
 if __name__ == "__main__":
@@ -120,25 +206,11 @@ if __name__ == "__main__":
     parser.add_argument('--name', required=True, type=str, dest='name', help='name of folder timelapse to be saved in')
     parser.add_argument('--timelapsedir', required=True, type=str, dest='timelapse_dir', help='dir that holds created timelapse & output')
     parser.add_argument('--s3bucket', required=True, type=str, dest="s3bucket", help="name of the bucket where file will be uploaded and mux will download from")
+    parser.add_argument('--kafka_bootstrap_servers', required=True, type=str, dest="kafka_bootstrap_servers", help="Kafka bootstrap servers, e.g. localhost:9092")
 
     args = parser.parse_args()
 
-    
-    #openai_test()
+    timelapse_generator = TimelapseGenerator(args.name, args.timelapse_dir, args.s3bucket, args.kafka_bootstrap_servers)
+    timelapse_generator.run()
 
-    # use ffmpeg to generate video from directory of photos
-    #need to handle video overwrite...
-    generate_video_from_photos(args.timelapse_dir, args.name)
-    
-    # upload to s3
-    file_path = args.timelapse_dir + "/output/" + args.name + ".mp4"
-    upload_to_s3(file_path, args.s3bucket)
 
-    # have mux grab video & encode for streaming 
-    asset_playback_id = mux_download_from_s3(args.s3bucket, args.name)
-    
-    #what do I need to do with this playback ID??
-    
-
-    # delete file from bucket once uploaded??? 
-    # ... just make private preferably 
--- photographer/src/main.py
+++ photographer/src/main.py
@@ -1,103 +1,234 @@
 import gphoto2 as gp
-import os, sys, time
-import subprocess
+import os
+import sys
+import time
 import argparse
 import logging
 from loguru import logger
-import fastapi
-import random
+import base64
+from confluent_kafka import Producer, KafkaError
+
 gp.error_severity[gp.GP_ERROR] = logging.DEBUG
 
-# create the directory that will house photos captured by the timelapse 
-def create_timelapse_dir(output_path, tl_name):
-    try:
-        print('creating timelapse directory')
-        original_umask = os.umask(0)
-        os.mkdir(output_path + '/' + tl_name, 0o777)
-        os.mkdir(output_path + '/' + tl_name + '/capture', 0o777)
-        os.mkdir(output_path + '/' + tl_name + '/output', 0o777)
 
-        
-    except OSError as ose:
-        if not os.listdir(output_path + '/' + tl_name + '/capture'):
-            print('Directory with name %s exists, but capture directory is empty.  Creating timelapse...' % tl_name)
-        else: 
-            print('Timelapse with the name %s already exists' % tl_name)
+class Timelapse:
+    def __init__(self, output_path, tl_name, interval, frames, kafka_bootstrap_servers):
+        self.output_path = output_path
+        self.tl_name = tl_name
+        self.interval = interval
+        self.frames = frames
+        self.capture_path = os.path.join(self.output_path, self.tl_name, 'capture')
+        self.kafka_bootstrap_servers = kafka_bootstrap_servers
+        self.producer = self.create_producer()
+
+
+    def create_timelapse_dir(self):
+        """
+        Create the directory structure for the timelapse.
+        """
+        try:
+            logger.info('Creating timelapse directory...')
+            original_umask = os.umask(0)
+            os.makedirs(os.path.join(self.output_path, self.tl_name, 'capture'), 0o777, exist_ok=True)
+            os.makedirs(os.path.join(self.output_path, self.tl_name, 'output'), 0o777, exist_ok=True)
+        except OSError as ose:
+            logger.info(f'Timelapse with the name {self.tl_name} already exists')
             sys.exit(1)
-    else:
-        print('Creating timelapse... Photos located under /%s/capture ' % tl_name)
-    finally:
-        os.umask(original_umask)
+        else:
+            logger.info(f'Creating timelapse... Photos located under {self.tl_name}/capture')
+        finally:
+            os.umask(original_umask)
 
-    return 
+    def connect_camera(self):
+        """
+        Connect to the camera and return the camera object.
+        """
+        camera = gp.Camera()
 
-# create camera instance, detect connected cameras via usb
-def connect_camera():
-    camera = gp.Camera()
-
-    while True:
-        try:
-            camera.init()
-        except gp.GPhoto2Error as ex:
-            if ex.code == gp.GP_ERROR_MODEL_NOT_FOUND:
-                print("No camera connected - checking again in 10 seconds")
-                print("EX CODE: " + str(ex.code))
-                # no camera, try again in 10 seconds
-                time.sleep(10)
-                continue
-            # some other error we can't handle here
-            raise
-
-        # on connection, print camera information & exit loop
-        # re-add this as debug logging
-        print('Summary')
-        print('=======')
-        print(camera.get_summary())
-        break
-    
-    return camera
+        while True:
+            try:
+                camera.init()
+            except gp.GPhoto2Error as ex:
+                if ex.code == gp.GP_ERROR_MODEL_NOT_FOUND:
+                    logger.info("No camera connected - checking again in 10 seconds")
+                    time.sleep(10)
+                    continue
+                raise
+            
+            # Set the camera to capture in JPEG format and 3:2 aspect ratio
+            try:
+                self.set_camera_capture_format(camera, "JPEG")
+                self.set_camera_aspect_ratio(camera, "3:2")
+            except gp.GPhoto2Error as ex:
+                logger.error(f"Failed to set camera configuration: {str(ex)}")
+                raise
 
 
-def execute_timelapse(camera, capture_path, interval, frames):
 
-    for x in range(frames):
-        try:
-            print('starting capture')
-            file_path = camera.capture(gp.GP_CAPTURE_IMAGE)
-            print('Camera file path: {0}/{1}'.format(file_path.folder, file_path.name))
-            target = os.path.join(capture_path, file_path.name)
-            print('Copying image to', target)
-            camera_file = camera.file_get(
-            file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL)
-            camera_file.save(target)
-            time.sleep(interval)
-        except Exception as e: print(e)
+            logger.info('Summary')
+            logger.info('=======')
+            logger.info(camera.get_summary())
+            break
+
+        return camera
+
+    def set_camera_capture_format(self, camera, format):
+        format_mapping = {
+            "JPEG": "Standard",
+            "RAW": "RAW",
+            "RAW+JPEG": "RAW & JPEG"
+        }
+
+        if format not in format_mapping:
+            raise ValueError(f"Invalid format '{format}'. Supported formats are: {', '.join(format_mapping.keys())}")
+
+        config = camera.get_config()
+        capture_format = config.get_child_by_name("imagequality")
+
+        target_format = format_mapping[format]
+
+        for i in range(capture_format.count_choices()):
+            choice_value = capture_format.get_choice(i)
+            if choice_value == target_format:
+                capture_format.set_value(choice_value)
+                camera.set_config(config)
+                logger.info(f"Camera capture format set to {format}")
+                break
+
+    def set_camera_aspect_ratio(self, camera, aspect_ratio):
+        aspect_ratios = ["3:2", "16:9"]
+
+        if aspect_ratio not in aspect_ratios:
+            raise ValueError(f"Invalid aspect ratio '{aspect_ratio}'. Supported aspect ratios are: {', '.join(aspect_ratios)}")
+
+        config = camera.get_config()
+        aspect_ratio_config = config.get_child_by_name("aspectratio")
+
+        for i in range(aspect_ratio_config.count_choices()):
+            choice_value = aspect_ratio_config.get_choice(i)
+            if choice_value == aspect_ratio:
+                aspect_ratio_config.set_value(choice_value)
+                camera.set_config(config)
+                logger.info(f"Camera aspect ratio set to {aspect_ratio}")
+                break
+
+    def create_producer(self):
+        conf = {
+                'bootstrap.servers': self.kafka_bootstrap_servers,
+                'message.max.bytes':1000000000
+            }
+        producer = Producer(conf)
+        return producer
+
+    def send_message(self, topic, value):
+        self.producer.produce(topic, value)
+        self.producer.flush()
+
+    def execute_timelapse(self, camera):
+        """
+        Execute the timelapse by taking photos with the given camera.
+        """
+
+        # Initialize the Kafka producer
+        producer = self.create_producer
+
+        for x in range(self.frames):
+            max_retries = 3
+            for attempt in range(1, max_retries + 1):
+                try:
+
+                    logger.info('Starting capture...')
+                    file_path = camera.capture(gp.GP_CAPTURE_IMAGE)
+                    logger.info(f'Camera file path: {file_path.folder}/{file_path.name}')
+                    target = os.path.join(self.capture_path, file_path.name)
+                    #logger.info(f'Copying image to {target}')
+                    camera_file = camera.file_get(file_path.folder, file_path.name, gp.GP_FILE_TYPE_NORMAL)
+                    # camera_file.save(target)
+                    logger.info('Copy complete')
+
+                    logger.info('Start send to Kafka queue')
+                    # Encode the image data in base64
+                    img_data = camera_file.get_data_and_size()
+                    img_base64 = base64.b64encode(img_data)
+                    logger.info('image encoded')
+
+                    # Send the base64-encoded image data to the Kafka queue
+                    #producer.send('timelapse_images', img_base64)
+                     #handle kafak upload
+                    def on_send_success(record_metadata):
+                        logger.info(f"Message sent successfully to topic {record_metadata.topic} at offset {record_metadata.offset}")
+
+                    def on_send_error(excp):
+                        logger.error(f"Error while sending message to Kafka: {excp}", exc_info=True)
+
+ 
+                    try:
+                        # Send the base64-encoded image data to the Kafka queue
+                        #future = self.send_message('timelapse_images2', img_base64)
+                        self.send_message('timelapse_images2', img_base64)
+                        # Add the success and error callbacks
+                        #future.add_callback(on_send_success)
+                        #future.add_errback(on_send_error)
+                        self.producer.flush()
+                    except AssertionError as e:
+                        logger.error(f"Error in file format potentially??!!??: {e}", exc_info=True)
+                    except KafkaError as e:
+                        logger.error(f"Error during timelapse execution: {e}", exc_info=True)
+
+                    logger.info('Image sent to Kafka queue')
+
+
+                    #Wait for the camera to be ready for capture, but retry photo if max retries hit
+                    max_retries_capture_readiness = 3
+                    for attempt in range(1, max_retries_capture_readiness + 1):
+                        event_type, event_data = camera.wait_for_event(1000)
+                        if event_type == gp.GP_EVENT_TIMEOUT:
+                            logger.warning("Camera not ready, waiting...attempt number" + str(attempt))
+                            continue
+
+                    time.sleep(self.interval)
+                    logger.info("Sleeping for interval:" + str(self.interval))
+                    break  # Break the retry loop if the capture is successful
+
+                except KafkaError as e:
+                    logger.error(f'Error sending image to Kafka queue: {e}')    
+                except gp.GPhoto2Error as e:
+                    if e.code == gp.GP_ERROR_IO:
+                        logger.warning(f'I/O problem (error code: {e.code}). Attempt {attempt} of {max_retries}')
+                        if attempt < max_retries:
+                            time.sleep(5)  # Wait for 5 seconds before retrying
+                        else:
+                            logger.error(f'Capture failed after {max_retries} attempts due to I/O problem')
+                    else:
+                        logger.error(e)
+                except BaseException as e:
+                    logger.error(str(e))
+
+
+    def run(self):
+        # try:
+            self.create_timelapse_dir()
+            camera = self.connect_camera()
+            try:
+                self.execute_timelapse(camera)
+            except Exception as e:
+                logger.error(f"Error during timelapse execution: {e}")
+            finally:
+                camera.exit()
+        # except Exception as e:
+        #     logger.error(f"Unexpected error: {e}")
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser(description='Timelapse via picamera or gphoto2')
 
-    parser.add_argument('--name', required=True, type=str, dest='name', help='name of folder timelapse to be saved in')
-    parser.add_argument('--outputpath', required=True, type=str, dest='output_path', help='output dir that will hold newly created timelapse dir')
-    parser.add_argument('--frames', required=True, type=int, dest='frames',
-                    help='total number of photos to be taken')
-    parser.add_argument('--interval', required=True, dest='interval', type=int,
-                    help='time between each photo in seconds')
+    parser.add_argument('--name', required=True, type=str, dest='name', help='Name of folder timelapse to be saved in')
+    parser.add_argument('--outputpath', required=True, type=str, dest='output_path', help='Output dir that will hold newly created timelapse dir')
+    parser.add_argument('--frames', required=True, type=int, dest='frames', help='Total number of photos to be taken')
+    parser.add_argument('--interval', required=True, dest='interval', type=int, help='Time between each photo in seconds')
+    parser.add_argument('--kafka-bootstrap-servers', required=True, type=str, dest='kafka_bootstrap_servers',
+                            help='Kafka bootstrap servers (e.g., "localhost:9092")')
 
     args = parser.parse_args()
-    print(args)
 
-    #create directory
-    capture_path = os.path.join(args.output_path, args.name, 'capture')
-    print(capture_path)
-    create_timelapse_dir(args.output_path, args.name)
-
-    #create camera instance
-    camera = connect_camera()
-
-    #run timelapse sequence
-    execute_timelapse(camera, capture_path, args.interval, args.frames)
-
-    #exit camera instance
-    camera.exit()
-
+    timelapse = Timelapse(args.output_path, args.name, args.interval, args.frames, args.kafka_bootstrap_servers)
+    timelapse.run()
